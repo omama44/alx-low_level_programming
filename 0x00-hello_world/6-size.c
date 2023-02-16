7662f2cb518a8e9e656b50fc02bf0cdd3b8ab8b8
omama44 <emy4495.m@gmail.com>
2023-02-16 05:40:48 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ char a;
+ int b;
+ long int c;
+ float f;
+ printf("
+     	return (0);
+}
+
