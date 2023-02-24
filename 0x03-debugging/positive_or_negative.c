d5dc7db50930d27cb0dcf316b1e5c7b6afe2a46b
omama44 <emy4495.m@gmail.com>
2023-02-23 18:50:13 -0800

    tast

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+
+	return (0);
+}
