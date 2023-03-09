69e13af14fd136d724e3286e991d33539d9f8ed9
omama44 <emy4495.m@gmail.com>
2023-03-09 07:26:23 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * sqrt_mod - Recursive function to find a square root
+ * of a given number
+ * @n: Given number
+ * @i: Variable counter
+ * Return: Square root of a given number
+ */
+int sqrt_mod(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	i++;
+	if (i * i < n)
+		return (sqrt_mod(n, ++i));
+	if (i * i > n)
+		return (-1);
+	return (i);
+}
+/**
+ * _sqrt_recursion - Square root of a given number
+ * @n: Integer variable
+ * Return: Integer variable
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n == 1 || n == 0)
+		return (n);
+
+	return (sqrt_mod(n, i));
+}
