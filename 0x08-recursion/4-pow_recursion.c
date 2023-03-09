37ca11f915f8dafaa28213b4d24051a20ff7282c
omama44 <emy4495.m@gmail.com>
2023-03-09 09:36:11 -0800

    returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -19,17 +19,3 @@ int sqrt_mod(int n, int i)
 		return (-1);
 	return (i);
 }
-/**
- * _sqrt_recursion - Square root of a given number
- * @n: Integer variable
- * Return: Integer variable
- */
-int _sqrt_recursion(int n)
-{
-	int i = 1;
-
-	if (n == 1 || n == 0)
-		return (n);
-
-	return (sqrt_mod(n, i));
-}
