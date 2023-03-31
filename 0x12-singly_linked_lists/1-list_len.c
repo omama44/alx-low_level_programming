98fb962c7a752ba48ef3d6bffd62e44437bf3342
omama44 <emy4495.m@gmail.com>
2023-03-31 05:56:55 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,51 +1,22 @@
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
-  * add_node - Adds a new node at the beginning of a list
-  * @head: The original linked list
-  * @str: The string to add to the node
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
   *
-  * Return: The address of the new list or NULL if it failed
+  * Return: The number of elements of a list
   */
-list_t *add_node(list_t **head, const char *str)
+size_t list_len(const list_t *h)
 {
-	list_t *temp;
+	size_t count = 0;
 
-	if (head != NULL && str != NULL)
+	while (h)
 	{
-		temp = malloc(sizeof(list_t));
-		if (temp == NULL)
-			return (NULL);
-
-		temp->str = strdup(str);
-		temp->len = _strlen(str);
-		temp->next = *head;
-
-		*head = temp;
-
-		return (temp);
+		h = h->next;
+		count++;
 	}
 
-	return (0);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (*s)
-	{
-		s++;
-		c++;
-	}
-
-	return (c);
+	return (count);
 }
